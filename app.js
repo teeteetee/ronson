f4b0a82c0e608fa65994c99ec37681527dba11fe
Toldya <fuckoff@already.com>
2015-11-22 12:09:04 -0800

    --

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ var bodyParser = require('body-parser');
 var sessions = require('client-sessions');
 var Cookies = require('cookies');
 var bcrypt = require('bcrypt');
+var nodemailer = require("nodemailer");
 var ObjectID = require('mongodb').ObjectID;
 var http = require('http');
 //var gm = require('gm').subClass({imageMagick: true}); - crashes , no binaries found
 var gm = require('gm');
-var nodemailer = require('nodemailer');
 
-
-var mongo = require('mongodb').MongoClient;
-var db = require('monk')('localhost/tav')
-  , users = db.get('users',user_messages = db.get('user_messages');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/tav'),users = db.get('users'),user_messages = db.get('user_messages');
 // POSTS and OBJECTS BELONGS TO MALESHIN PROJECT DELETE WHEN PUSHING TOPANDVIEWS TO PRODUCTION
 var fs = require('fs-extra');
 
